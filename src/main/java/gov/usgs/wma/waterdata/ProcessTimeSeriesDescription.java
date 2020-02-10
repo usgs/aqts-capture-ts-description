64b9801e91763d55cc74a90606213a96cc142e29
ssoper <ssoper@usgs.gov>
2020-02-10 11:30:36 -0600

    Add a real logger

--- src/main/java/gov/usgs/wma/waterdata/ProcessTimeSeriesDescription.java
+++ src/main/java/gov/usgs/wma/waterdata/ProcessTimeSeriesDescription.java
@@ -3,14 +3,18 @@ package gov.usgs.wma.waterdata;
 import java.util.List;
 import java.util.function.Function;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProcessTimeSeriesDescription implements Function<RequestObject, Object> {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ProcessTimeSeriesDescription.class);
+
 	@Autowired
-	TimeSeriesDescriptionDao tsdDao;
+	TimeSeriesDescriptionDao timeSeriesDescriptionDao;
 
 	@Override
 	public  ResultObject apply(RequestObject request) {
@@ -20,12 +24,12 @@ public class ProcessTimeSeriesDescription implements Function<RequestObject, Obj
 	protected ResultObject processRequest(RequestObject request) {
 		ResultObject result = new ResultObject();
 
-		// run the insert
-		System.out.println("Here's the input: ");
-		System.out.println(request.getId());
-		List<String> tsduids = tsdDao.upsertTimeSeriesDescriptionsForSingleJsonDataId(request.getId());
-
-		result.setUniqueIds(tsduids);
+		// The upsert returns a list of time series unique ids that were either updated or inserted.  Returns an empty
+		// list if no records were updated or inserted.
+		LOG.info("json_data_id: {}", request.getId());
+		List<String> uniqueIds = timeSeriesDescriptionDao.upsertTimeSeriesDescriptionsForSingleJsonDataId(request.getId());
+		LOG.info("uniqueIds: {}", uniqueIds);
+		result.setUniqueIds(uniqueIds);
 		return result;
 	}
 }
